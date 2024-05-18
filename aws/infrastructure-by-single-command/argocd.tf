0196ec264cac2c3b81973476fb1dc5f85958850d
Vladyslav Liubenok <Vladyslav.Liubenok@example.com>
2024-05-18 19:57:13 +0300

    update

--- aws/infrastructure-by-single-command/argocd.tf
+++ aws/infrastructure-by-single-command/argocd.tf
@@ -7,7 +7,7 @@ provider "helm" {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id, "--region", var.aws_region]
+      args = ["eks", "get-token", "--cluster-name", "demo-eks-cluster", "--region", var.aws_region]
     }
   }
 }
